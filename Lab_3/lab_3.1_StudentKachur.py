970f247d9ab2b462870f9d153cfcb45976fb7c1e
ArtemKachur7 <kachurartem7@gmail.com>
2023-09-19 21:00:31 +0300

    Лабораторна робота №3

---
+++ Lab_3/lab_3.1_StudentKachur.py
@@ -0,0 +1,8 @@
+var_john = 3 #Кількість яблук у Джона
+var_mary = 5 #Кількість яблук у Мері
+var_adam = 6 #Кількість яблук у Адама
+print(var_john,",",var_mary,",",var_adam) #Кількість яблук у кожного з дітей через кому
+totalApples = var_john+var_mary+var_john #Загальна кількість яблук у дітей
+print(totalApples)
+print("Загальна кілкьість яблук у дітей :",totalApples)
+
