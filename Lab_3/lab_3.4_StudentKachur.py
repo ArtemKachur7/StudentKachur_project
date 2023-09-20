0efd0fdaa4b4609ebab867bc53d21969f3d35b6b
ArtemKachur7 <kachurartem7@gmail.com>
2023-09-20 09:46:28 +0300

    Лабораторна робота №3

---
+++ Lab_3/lab_3.4_StudentKachur.py
@@ -0,0 +1,9 @@
+# this program computes the number of seconds in a given number of hours
+
+num_hours = 2 # number of hours
+num_seconds = 3600 # number of seconds in 1 hour
+
+print("Hours: ", num_hours) #printing the number of hours
+print("Seconds in Hours: ", num_hours * num_seconds) # printing the number of seconds in a given number of hours
+print("Seconds in 3 Hour: ", (num_hours+1)*num_seconds)#this is the end of the program that computes the number of seconds in 3 hour
+print("Goodbye")
