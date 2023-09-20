59fd7b5f79f57b22b6fa7cc4518893e098e3377c
ArtemKachur7 <kachurartem7@gmail.com>
2023-09-20 09:59:07 +0300

    Лабораторна робота №3

---
+++ Lab_3/lab_3.5_StudentKachur.py
@@ -0,0 +1,9 @@
+var_a=float(input("Введіть змінну а: "))# input a float value for variable a here
+var_b=float(input("Введіть змінну b: "))# input a float value for variable b here
+
+print("Сума змінних а і b: ",var_a+var_b)# output the result of addition here
+print("Різниця змінних а і b: ","a-b=",var_a-var_b,";","b-a=",var_b-var_a)# output the result of subtraction here
+print("Добуток змінних а і b: ",var_a*var_b)# output the result of multiplication here
+print("Частка змінних а і b: ","a/b=",var_a/var_b,";","b/a=",var_b/var_a)# output the result of division here
+
+print("\nThat's all, folks!")
