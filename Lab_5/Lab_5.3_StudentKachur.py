2b6db7d74e3d7442aecf770fb9d2204f12cd9b98
ArtemKachur7 <kachurartem7@gmail.com>
2023-10-22 22:22:46 +0300

    Лабораторна робота №5

--- Lab_5/Lab_5.3_StudentKachur.py
+++ Lab_5/Lab_5.3_StudentKachur.py
@@ -1,4 +1,4 @@
-matrix_of_chessboard = [['_'] * 8 for _ in range(8)]# Створюємо матрицю 8x8 з пустими клітинками
+matrix_of_chessboard = [['_'] * 8 for _ in range(8)] # Створюємо матрицю 8x8 з пустими клітинками
 # Розставляємо тури по кутках шахівниці:
 matrix_of_chessboard[0][0] = 'Т' # Лівий верхній кут
 matrix_of_chessboard[0][7] = 'Т(білі)' # Правий верхній кут
