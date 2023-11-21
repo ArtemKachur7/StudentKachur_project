76fe9421d315436f6d52410aefbd1c0f60f8e3b2
ArtemKachur7 <kachurartem7@gmail.com>
2023-11-21 10:58:30 +0200

    Лабораторна робота №6

---
+++ Lab_6/Lab_6_StudentKachur.py
@@ -0,0 +1,23 @@
+def is_year_leap(year): #Функція на перевірку чи високосний рік , якщо він високосний він без залишку ділиться на 4 , 100 або 400
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+#тестуючий код
+test_data = [1900, 2000, 2016, 1987]
+test_results = [False, True, True, False]
+for i in range(len(test_data)):
+    yr = test_data[i]
+    print(yr,"->",end="")
+    result = is_year_leap(yr)
+    if result == test_results[i]:
+        print("OK")
+    else:
+        print("Failed")
