abb4dfbf48737ff66089cf96acfb1753832a2305
ArtemKachur7 <kachurartem7@gmail.com>
2023-11-22 11:10:40 +0200

    Лабораторна робота №8

---
+++ Lab_8/main2.py
@@ -0,0 +1,26 @@
+from sys import path
+path.append('..\\packages')
+
+import extra.iota
+
+print(extra.iota.FunI())
+
+
+import extra.good.best.sigma
+from extra.good.best.tau import FunT
+
+print(extra.good.best.sigma.FunS())
+print(FunT())
+
+from sys import path
+path.append('..\\packages\\Extrapack ZIP file.zip')
+
+import extra.good.best.sigma as sig
+import extra.good.alpha as alp
+from extra.iota import FunI
+from extra.good.beta import FunB
+
+print(sig.FunS())
+print(alp.FunA())
+print(FunI())
+print(FunB())
